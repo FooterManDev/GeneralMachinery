9d79edd9507fdd9fb2bac697d30348f8a107facb
footerman <footerman.work@gmail.com>
2024-12-22 10:35:37 -0500

    Initial commit

---
+++ src/main/kotlin/io/github/footerman/generalmachinery/Main.kt
@@ -0,0 +1,10 @@
+package io.github.footerman.generalmachinery
+
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.common.Mod
+
+@Mod(Main.ID)
+object Main
+{
+    const val ID = "generalmachinery"
+}
